58dc27952ae1453de95825c30871e63aa3a143e6
pomponiosimonegit config --global usern.email pomponio671@gmail.com <Pomponio671@gmail.com>
2024-06-10 15:35:14 +0200

    esercizio 1 e 2 fatti

---
+++ classe.js
@@ -0,0 +1,29 @@
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+  static isSameAuthorStatic(user1, user2) {
+    if (user1.age > user2.age) {
+      return `${user1.firstName} è più grande di ${user2.firstName}`;
+    } else if (user1.age < user2.age) {
+      return `${user2.firstName} è più grande di ${user1.firstName}`;
+    } else user1.age === user2.age;
+    {
+      return `${user1.firstName} e ${user2.firstName} hanno la stessa età`;
+    }
+  }
+}
+const primoUser = new User("Giacomo", "Biondi", 29, "Roma");
+console.log(primoUser);
+
+const secondoUser = new User("Pierluigi", "Ambrogio", 40, "Venezia");
+console.log(secondoUser);
+
+const terzoUser = new User("Simone", "Ambrogio", 40, "Milano");
+console.log(secondoUser);
+
+console.log(User.isSameAuthorStatic(primoUser, secondoUser));
+console.log(User.isSameAuthorStatic(secondoUser, terzoUser));
---
+++ form.js
@@ -0,0 +1,47 @@
+const form = document.querySelector("form");
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+  static isSameAuthorStatic(pet1, pet2) {
+    return pet1.ownerName === pet2.ownerName;
+  }
+}
+
+const pets = [];
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+
+  const petName = document.getElementById("PetName").value;
+  const ownerName = document.getElementById("OwnerName").value;
+  const species = document.getElementById("Species").value;
+  const breed = document.getElementById("Breed").value;
+
+  const newPet = new Pet(petName, ownerName, species, breed);
+  pets.push(newPet);
+
+  console.log("PetName:", petName);
+  console.log("OwnerName:", ownerName);
+  console.log("Species:", species);
+  console.log("Breed:", breed);
+
+  const listItem = document.createElement("li");
+
+  listItem.textContent = `Pet Name: ${petName}, Owner Name: ${ownerName}, Species: ${species}, Breed: ${breed}`;
+
+  petList.appendChild(listItem);
+
+  pets.forEach((pet) => {
+    if (pet !== newPet && Pet.isSameAuthorStatic(pet, newPet)) {
+      console.log(
+        ` ${newPet.petName} e ${pet.petName} hanno lo stesso padrone : ${ownerName}`
+      );
+    }
+  });
+
+  form.reset();
+};
